cc98500b3b481b7b6edcdd33a9e95902fac9fffb
NovoxMx <angelfidelmg03@gmail.com>
2025-04-11 11:37:25 -0700

    Cambio en el lado derecho de las skills para que tuvieran la animacion de decrypted

--- src/app/components/rankingright.tsx
+++ src/app/components/rankingright.tsx
@@ -1,34 +1,35 @@
 import RakingBar from "../constants/rankingbar";
+import Decrypted from "./decrypted";
 
 export default function RankingRight() {
     return (
 
         <div className="w-3/4 mr-6 flex flex-col justify-items-start">
             <div className="flex items-center justify-between w-full mr-4">
-                <h3 className="text-lg font-MMomo text-gray-950">React</h3>
+                <Decrypted> React </Decrypted>
                 <h6 className="text-sm font-MMomo text-gray-950 mr-20">3 year exp.</h6>
             </div>
             <RakingBar rank={5} position={2} />
 
             <div className="flex items-center justify-between w-full mt-4">
-                <h3 className="text-lg font-MMomo text-gray-950">Next</h3>
+                <Decrypted> Next </Decrypted>
                 <h6 className="text-sm font-MMomo text-gray-950 mr-20">2 year exp.</h6>
             </div>
             <RakingBar rank={4} position={2} />
 
             <div className="flex items-center justify-between w-full mt-4">
-                <h3 className="text-lg font-MMomo text-gray-950">Figma</h3>
+                <Decrypted> Figma </Decrypted>
                 <h6 className="text-sm font-MMomo text-gray-950 mr-20">3 year exp.</h6>
             </div>
             <RakingBar rank={4} position={2} />
             <div className="flex items-center justify-between w-full mt-4">
-                <h3 className="text-lg font-MMomo text-gray-950">TypeScript</h3>
+                <Decrypted> TypeScript </Decrypted>
                 <h6 className="text-sm font-MMomo text-gray-950 mr-20">2 year exp.</h6>
             </div>
             <RakingBar rank={4} position={2} />
 
             <div className="flex items-center justify-between w-full mt-4">
-                <h3 className="text-lg font-MMomo text-gray-950">Tailwind</h3>
+                <Decrypted> Tailwind </Decrypted>
                 <h6 className="text-sm font-MMomo text-gray-950 mr-20">2 year exp.</h6>
             </div>
             <RakingBar rank={4} position={2} />
