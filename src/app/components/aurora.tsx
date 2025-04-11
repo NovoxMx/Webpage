c9d8bac9543c74dce4e78425d8d2746a78a243ba
NovoxMx <angelfidelmg03@gmail.com>
2025-04-11 11:19:26 -0700

    Cambiar la declaración de variable 'program' de let a const en el componente Aurora para mejorar la claridad del código.

--- src/app/components/aurora.tsx
+++ src/app/components/aurora.tsx
@@ -141,7 +141,7 @@ export default function Aurora(props: AuroraProps) {
     gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
     gl.canvas.style.backgroundColor = "#000000";
 
-    let program: Program | undefined;
+    const program: Program | undefined;
 
     function resize() {
       if (!ctn) return;
