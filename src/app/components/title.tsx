55cff8a290ce96c897133d2b1e3b358f3df2ca21
NovoxMx <angelfidelmg03@gmail.com>
2025-05-02 16:40:53 -0700

    fix: remove any from children

--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -1,4 +1,4 @@
-export default function Title({ children }: { children: any }) {
+export default function Title({ children }: { children: string }) {
     return (
         <div className="flex justify-center mt-6">
             <h1 className="text-2xl font-MPMomo text-emerald-500">{children}</h1>
