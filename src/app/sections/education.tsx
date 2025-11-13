633ce535bc41a0de61bc19dcffc716aa51146dd0
NovoxMx <angelfidelmg03@gmail.com>
2025-11-13 07:58:30 -0700

    Fix: Name of the institution

--- src/app/sections/education.tsx
+++ src/app/sections/education.tsx
@@ -9,7 +9,7 @@ export default function Education() {
             <Title> Education </Title>
             <div className="flex justify-around mt-10 mb-10">
                 <p className="text-lg font-MMomo text-gray-700 text-justify w-4/7">
-                    I am a Software Engineering student at the Monterrey Institute of Technology and Higher Education (ITESM) in Hermosillo, Sonora, Mexico. I have been studying here since 2021, and during this time, I have developed a solid foundation in various techniques, such as programming languages, algorithms, data structures, and software development. I am passionate about learning new technologies, particularly DevSecOps. Additionally, I actively participate in extracurricular activities such as programming competitions and hackathons, where I can showcase my skills and collaborate with other students.
+                    I am a Software Engineering student at the Instituto Tecnológico y de Estudios Superiores de Monterrey (ITESM) in Hermosillo, Sonora, Mexico. I have been studying here since 2021, and during this time, I have developed a solid foundation in various techniques, such as programming languages, algorithms, data structures, and software development. I am passionate about learning new technologies, particularly in DevSecOps & FrontEnd development. Additionally, I actively participate in extracurricular activities such as programming competitions and hackathons, where I can showcase my skills and collaborate with other students.
 
                 </p>
                 <Image
