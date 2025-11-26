fe9025d2a27f28d67b13629d45b0ab36234824b9
NovoxMx <angelfidelmg03@gmail.com>
2025-11-26 13:30:05 -0700

    Add: implement mobile detection and display PhoneWarning component and adding more segment to the page (badge, contact, phonewarning)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+
+import { useState, useEffect } from "react";
+
 import Background from "./constants/background";
 import Navbar from "./sections/navbar";
 import About from "./sections/About";
@@ -6,22 +9,54 @@ import Skills from "./sections/skills";
 import Education from "./sections/education";
 import ScrollDown from "./constants/scrolldown";
 import Project from "./sections/projects";
-
+import Experience from "./sections/experience";
+import Badges from "./sections/Badges";
+import PhoneWarning from "./components/phonewarning";
+import Contact from "./sections/contact";
 
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768); // w < 768px = modo móvil
+    };
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
+
+  if (isMobile) return <PhoneWarning />;
+
   return (
     <div>
       <Navbar />
       <About />
       <Skills />
+
       <ScrollDown baseOpacity={0.5}>
         <Education />
       </ScrollDown>
+
       <ScrollDown baseOpacity={0.5}>
         <Project />
       </ScrollDown>
+
+      <ScrollDown baseOpacity={0.5}>
+        <Experience />
+      </ScrollDown>
+
+      <ScrollDown baseOpacity={0.5}>
+        <Badges />
+      </ScrollDown>
+
+      <ScrollDown baseOpacity={0.5}>
+        <Contact />
+      </ScrollDown>
+
       <Background />
     </div>
-
   );
 }
