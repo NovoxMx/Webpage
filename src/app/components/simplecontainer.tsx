045df37fbecbae63a8d028a078598bd833b51826
NovoxMx <angelfidelmg03@gmail.com>
2025-11-26 13:35:05 -0700

    Add: allow optional className prop in SimpleContainer for customizable styling

--- src/app/components/simplecontainer.tsx
+++ src/app/components/simplecontainer.tsx
@@ -1,8 +1,8 @@
-export default function SimpleContainer({ children }: { children: React.ReactNode }) {
+export default function SimpleContainer({ children, className }: { children: React.ReactNode, className?: string }) {
     return (
         <div className="flex items-center justify-center">
             <div className="w-3/4 h-full">
-                <div className="flex flex-col mt-6 bg-white rounded-sm">
+                <div className={`flex flex-col mt-6 bg-white rounded-sm ${className}`}>
                     {children}
                 </div>
             </div>
