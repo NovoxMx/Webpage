25b2eae821069d930016696959af4b83e9bec823
NovoxMx <angelfidelmg03@gmail.com>
2025-11-26 13:38:04 -0700

    Refactor: update Education component to use dynamic content based on language context

--- src/app/sections/education.tsx
+++ src/app/sections/education.tsx
@@ -1,17 +1,22 @@
-import csn from '../../../public/images/csn_tec.jpg';
-import Image from 'next/image';
-import SimpleContainer from '../components/simplecontainer';
-import Title from '../components/title';
+import csn from "../../../public/images/csn_tec.jpg";
+import Image from "next/image";
+import SimpleContainer from "../components/simplecontainer";
+import Title from "../components/title";
+import { getEducation } from "../utils/getEducation";
+import type { Education } from "../data/interfaces";
+import { useLanguage } from "../context/languageContext";
 
 export default function Education() {
+    const { lang } = useLanguage();
+
+    const eduList: Education[] = getEducation(lang);
+
     return (
         <SimpleContainer>
-            <Title> Education </Title>
-            <div className="flex justify-around mt-10 mb-10">
-                <p className="text-lg font-MMomo text-gray-700 text-justify w-4/7">
-                    I am a Software Engineering student at the Instituto Tecnológico y de Estudios Superiores de Monterrey (ITESM) in Hermosillo, Sonora, Mexico. I have been studying here since 2021, and during this time, I have developed a solid foundation in various techniques, such as programming languages, algorithms, data structures, and software development. I am passionate about learning new technologies, particularly in DevSecOps & FrontEnd development. Additionally, I actively participate in extracurricular activities such as programming competitions and hackathons, where I can showcase my skills and collaborate with other students.
+            <Title>{lang === "en" ? "Education" : "Educación"}</Title>
 
-                </p>
+            <div className="flex justify-around mt-10 mb-10">
+                <p className="text-lg font-MMomo text-gray-700 text-justify w-4/7"> {eduList[0].content} </p>
                 <Image
                     src={csn}
                     alt="CSN"
