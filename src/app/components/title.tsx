98b48374815d3114bd5c3ec5469d737038af75d9
NovoxMx <A00835132@tec.mx>
2025-11-27 17:54:49 -0700

    fix: add optional className prop and update styling for Title component

--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -1,10 +1,10 @@
-export default function Title({ children }: { children: string }) {
+export default function Title({ children, className }: { children: string, className?: string }) {
     return (
-        <div className="flex justify-center mt-6">
-            <div className="flex items-center gap-4 w-full max-w-3xl px-4">
-                <span className="flex-1 h-px bg-emerald-500" />
-                <h1 className="text-2xl font-MPMomo text-emerald-500">{children}</h1>
-                <span className="flex-1 h-px bg-emerald-500" />
+        <div className="flex justify-center my-6">
+            <div className="flex items-center gap-12 w-full max-w-3/4 px-4">
+                <div className="flex-1 h-px bg-emerald-500" />
+                <h1 className={`text-4xl font-MPMomo text-emerald-500 ${className}`}>{children}</h1>
+                <div className="flex-1 h-px bg-emerald-500" />
             </div>
         </div>
     );
