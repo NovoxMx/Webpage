0ab047db52987bba3b6b238d8f570ff1d22c470c
NovoxMx <A00835132@tec.mx>
2025-11-30 15:27:35 -0700

    feat: Add support for dark mode in the RankingLeft and RankingRight components

--- src/app/components/rakingleft.tsx
+++ src/app/components/rakingleft.tsx
@@ -5,32 +5,32 @@ export default function RakingLeft() {
     return (
         <div className="w-3/4 ml-6 flex flex-col items-end">
             <div className="flex items-center justify-between w-full ml-4">
-                <Decrypted className="font-MMomo text-gray-950 text-sm"> 1 year exp. </Decrypted>
-                <Decrypted className="font-MMomo text-gray-950 text-md"> AWS </Decrypted>
+                <Decrypted className="font-MMomo text-gray-950 text-sm dark:text-white"> 1 year exp. </Decrypted>
+                <Decrypted className="font-MMomo text-gray-950 text-md dark:text-white"> AWS </Decrypted>
             </div>
             <RakingBar rank={3} position={1} />
 
             <div className="flex items-center justify-between w-full mt-4">
-                <Decrypted className="font-MMomo text-gray-950 text-sm"> 3 year exp. </Decrypted>
-                <Decrypted className="font-MMomo text-gray-950 text-md"> MySQL </Decrypted>
+                <Decrypted className="font-MMomo text-gray-950 text-sm dark:text-white"> 3 year exp. </Decrypted>
+                <Decrypted className="font-MMomo text-gray-950 text-md dark:text-white"> MySQL </Decrypted>
             </div>
             <RakingBar rank={3} position={1} />
 
             <div className="flex items-center justify-between w-full mt-4">
-                <Decrypted className="font-MMomo text-gray-950 text-sm"> 3 year exp. </Decrypted>
-                <Decrypted className="font-MMomo text-gray-950 text-md"> PHP </Decrypted>
+                <Decrypted className="font-MMomo text-gray-950 text-sm dark:text-white"> 3 year exp. </Decrypted>
+                <Decrypted className="font-MMomo text-gray-950 text-md dark:text-white"> PHP </Decrypted>
             </div>
             <RakingBar rank={1} position={1} />
 
             <div className="flex items-center justify-between w-full mt-4">
-                <Decrypted className="font-MMomo text-gray-950 text-sm"> 4 year exp. </Decrypted>
-                <Decrypted className="font-MMomo text-gray-950 text-md"> Python </Decrypted>
+                <Decrypted className="font-MMomo text-gray-950 text-sm dark:text-white"> 4 year exp. </Decrypted>
+                <Decrypted className="font-MMomo text-gray-950 text-md dark:text-white"> Python </Decrypted>
             </div>
             <RakingBar rank={4} position={1} />
 
             <div className="flex items-center justify-between w-full mt-4">
-                <Decrypted className="font-MMomo text-gray-950 text-sm"> 4 year exp.</Decrypted>
-                <Decrypted className="font-MMomo text-gray-950 text-md"> C++ </Decrypted>
+                <Decrypted className="font-MMomo text-gray-950 text-sm dark:text-white"> 4 year exp.</Decrypted>
+                <Decrypted className="font-MMomo text-gray-950 text-md dark:text-white"> C++ </Decrypted>
             </div>
             <RakingBar rank={4} position={1} />
         </div>
--- src/app/components/rankingright.tsx
+++ src/app/components/rankingright.tsx
@@ -6,31 +6,31 @@ export default function RankingRight() {
 
         <div className="w-3/4 mr-6 flex flex-col justify-items-start">
             <div className="flex items-center justify-between w-full mr-4">
-                <Decrypted className="font-MMomo text-gray-950 text-md"> React </Decrypted>
-                <Decrypted className="text-sm font-MMomo text-gray-950 mr-10"> 3 year exp. </Decrypted>
+                <Decrypted className="font-MMomo text-gray-950 text-md dark:text-white"> React </Decrypted>
+                <Decrypted className="text-sm font-MMomo text-gray-950 mr-10 dark:text-white"> 3 year exp. </Decrypted>
             </div>
             <RakingBar rank={5} position={2} />
 
             <div className="flex items-center justify-between w-full mt-4">
-                <Decrypted className="font-MMomo text-gray-950 text-md"> Next </Decrypted>
-                <Decrypted className="text-sm font-MMomo text-gray-950 mr-10"> 2 year exp. </Decrypted>
+                <Decrypted className="font-MMomo text-gray-950 text-md dark:text-white"> Next </Decrypted>
+                <Decrypted className="text-sm font-MMomo text-gray-950 mr-10 dark:text-white"> 2 year exp. </Decrypted>
             </div>
             <RakingBar rank={4} position={2} />
 
             <div className="flex items-center justify-between w-full mt-4">
-                <Decrypted className="font-MMomo text-gray-950 text-md"> Figma </Decrypted>
-                <Decrypted className="text-sm font-MMomo text-gray-950 mr-10"> 3 year exp. </Decrypted>
+                <Decrypted className="font-MMomo text-gray-950 text-md dark:text-white"> Figma </Decrypted>
+                <Decrypted className="text-sm font-MMomo text-gray-950 mr-10 dark:text-white"> 3 year exp. </Decrypted>
             </div>
             <RakingBar rank={4} position={2} />
             <div className="flex items-center justify-between w-full mt-4">
-                <Decrypted className="font-MMomo text-gray-950 text-md"> TypeScript </Decrypted>
-                <Decrypted className="text-sm font-MMomo text-gray-950 mr-10"> 3 year exp.</Decrypted>
+                <Decrypted className="font-MMomo text-gray-950 text-md dark:text-white"> TypeScript </Decrypted>
+                <Decrypted className="text-sm font-MMomo text-gray-950 mr-10 dark:text-white"> 3 year exp.</Decrypted>
             </div>
             <RakingBar rank={4} position={2} />
 
             <div className="flex items-center justify-between w-full mt-4">
-                <Decrypted className="font-MMomo text-gray-950 text-md"> Tailwind </Decrypted>
-                <Decrypted className="text-sm font-MMomo text-gray-950 mr-10"> 2 year exp.</Decrypted>
+                <Decrypted className="font-MMomo text-gray-950 text-md dark:text-white"> Tailwind </Decrypted>
+                <Decrypted className="text-sm font-MMomo text-gray-950 mr-10 dark:text-white"> 2 year exp.</Decrypted>
             </div>
             <RakingBar rank={4} position={2} />
         </div>
