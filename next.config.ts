84ddc061fd78c76a38dae52edad48e0904d4f9d7
NovoxMx <angelfidelmg03@gmail.com>
2026-01-18 10:28:06 -0700

    feat: i return to my first config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@
 const nextConfig = {
   images: {
     dangerouslyAllowSVG: true,
+    contentSecurityPolicy:
+      "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: "https",
@@ -9,28 +11,6 @@ const nextConfig = {
       },
     ],
   },
-
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: [
-              "default-src 'self'",
-              "img-src 'self' data: https://cdn.simpleicons.org",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
-              "style-src 'self' 'unsafe-inline'",
-              "font-src 'self' data:",
-              "connect-src 'self'",
-            ].join("; "),
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
-
