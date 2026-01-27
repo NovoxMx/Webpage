ee9a7e0dae17d5ec9c99fce80d77f77f0b0765ec
NovoxMx <angelfidelmg03@gmail.com>
2026-01-27 16:38:38 -0700

    feat: new margin style to have so similar in both lenguage

--- src/app/components/descriptor.tsx
+++ src/app/components/descriptor.tsx
@@ -2,7 +2,7 @@ export default function Descriptor({ text, onClick }: { text: string; onClick?: 
     const baseClasses = "rounded-lg mt-2 w-2/12 font-MMomo flex items-center justify-center mb-4";
     const staticClasses = "border border-emerald-500 bg-none";
     const clickableClasses = "border border-emerald-500 bg-none hover:bg-emerald-500 cursor-pointer transition ";
-    const textClasses = "text-lg text-center text-emerald-500 h-full w-full flex items-center justify-center";
+    const textClasses = "text-lg text-center text-emerald-500 h-full w-full flex items-center justify-center mx-2";
 
     return onClick ? (
         <button type="button" onClick={onClick} className={`${baseClasses} ${clickableClasses}`}>
